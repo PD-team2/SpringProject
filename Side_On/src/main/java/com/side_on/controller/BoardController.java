2b1678ff1581b8932b595949bdfd75b8185393f4
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-08-04 16:11:09 +0900

    Create BoardController.java

---
+++ Side_On/src/main/java/com/side_on/controller/BoardController.java
@@ -0,0 +1,29 @@
+package com.side_on.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class BoardController {
+
+	@RequestMapping("/board/FaQBoard")
+	public String FaQBoard() {
+	
+		return "board/FaQBoard";
+	}
+	
+	@RequestMapping("/board/contactEmail")
+	public String contactEmail() {
+	
+		return "board/contactEmail";
+	}
+	
+	@RequestMapping("/board/contactResult")
+	public String contactResult() {
+	
+		return "board/contactResult";
+	}
+}
