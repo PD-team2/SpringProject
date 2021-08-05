bbb564fcc06efaaf5db2c04f06f273060914a5be
yunassnn <kasspo20@gmail.com>
2021-08-06 02:39:59 +0900

    admin dashboard() 매핑, notice 상위 5개 노출 위한 Autowired 추가

--- Side_On/src/main/java/com/side_on/controller/HomeController.java
+++ Side_On/src/main/java/com/side_on/controller/HomeController.java
@@ -2,20 +2,31 @@ package com.side_on.controller;
 
 
 
+import java.util.List;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.side_on.dto.Member;
+import com.side_on.dto.Notice;
+import com.side_on.service.MemberService;
+import com.side_on.service.NoticeService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class HomeController {
+	
+	@Autowired
+	public NoticeService noticeService;
+	
 	@RequestMapping("/")
 	public String home(HttpServletRequest request) {
 		ServletContext application = request.getServletContext();
@@ -29,6 +40,13 @@ public class HomeController {
 	public String makePoll() {
 	
 		return "vote/makePoll";
+	}
+	
+	@RequestMapping("/admin/dashboard")
+	public String dashboard(Model model) {
+		List<Notice> noticeList = noticeService.noticeList();
+		model.addAttribute("noticeList", noticeList);
+		return "admin/dashboard";
 	}
 	
 }
