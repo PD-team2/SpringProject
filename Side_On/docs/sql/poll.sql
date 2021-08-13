64f4265311ace35873a1c7fb95972ef95cf1c093
nakwon3345 <84373983+nakwon3345@users.noreply.github.com>
2021-08-13 09:43:52 +0900

    기능구현 예시 데이터

--- Side_On/docs/sql/poll.sql
+++ Side_On/docs/sql/poll.sql
@@ -19,3 +19,11 @@ create table tblPollItem(
 
 -- 트표 저장 
 CREATE SEQUENCE tblpolllist_seq START WITH 1 INCREMENT BY 1 MAXVALUE 9999 CYCLE NOCACHE;
+
+-- 기능 구현 예시 데이터 삽입
+insert into TBLPOLLLIST values (22, '관심있는 프로그래밍 언어는?', sysdate, '21/09/01', 1, 1);
+insert into TBLPOLLITEM values (22, 4, '자바', 0);
+insert into TBLPOLLITEM values (22, 4, '파이썬', 0);
+insert into TBLPOLLITEM values (22, 4, '자바스크립트', 0);
+insert into TBLPOLLITEM values (22, 4, 'C언어', 0);
+commit;
