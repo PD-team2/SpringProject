a7afd9f39231ba5319408f5656e2910880350842
yunassnn <kasspo20@gmail.com>
2021-08-16 18:11:42 +0900

    notice_no -> recruit_num 으로 신고기능 수정

--- Side_On/src/main/java/com/side_on/dao/RestDao.java
+++ Side_On/src/main/java/com/side_on/dao/RestDao.java
@@ -19,12 +19,21 @@ public interface RestDao {
 	public void updateRestManageModal(String restNo, String restResult);
 
 	/** 경고 횟수 증가용 사용자 아이디 조회 */
-	public String selectRestUser(String restNo);
+	public String selectRestUserForCount(String restNo);
 	
 	/** 숨김 게시글 재게시용 글번호 조회 */
-	public String selectRestPostNo(String restNo);
+	public int selectRestPostNo(String restNo);
 
 	/** 신고 접수 */
-	public void insertRest(String noticeNo, String memberId, String reason);
+	public void insertRest(int recruit_num, String memberId, String reason);
+	
+	/** 경고 게시글 숨김 해제 */
+	public void updatePostCondition(int recruit_num);
 
+	/** 경고 게시글 작성자 아이디 select */
+	public String selectRestWriteUser(int recruit_num);
+	
+	/** 경고 횟수 추가 */
+	public void updateRestCount(String memberId);
+	
 }
