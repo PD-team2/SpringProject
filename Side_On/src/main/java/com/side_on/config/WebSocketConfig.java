c380da0ac9154f6bfef494871b73806f8857ea09
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-08-18 09:53:50 +0900

    Update WebSocketConfig.java

--- Side_On/src/main/java/com/side_on/config/WebSocketConfig.java
+++ Side_On/src/main/java/com/side_on/config/WebSocketConfig.java
@@ -1,21 +1,20 @@
 package com.side_on.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import com.side_on.handler.SocketHandler;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer{
-
+	
 	@Autowired
 	SocketHandler socketHandler;
+	
 	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -32,4 +31,5 @@ public class WebSocketConfig implements WebSocketConfigurer{
 //	container.setMaxBinaryMessageBufferSize(500000);
 //	return container;
 //	}
+	
 }
