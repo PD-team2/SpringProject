eafe31030f4272f6d7f7d5cc38ed76d3f3a27f44
nakwon3345 <84373983+nakwon3345@users.noreply.github.com>
2021-08-18 10:03:48 +0900

    Update DBConnectionMgr.java

--- Side_On/src/main/java/com/side_on/poll/DBConnectionMgr.java
+++ Side_On/src/main/java/com/side_on/poll/DBConnectionMgr.java
@@ -1,326 +1,293 @@
 package com.side_on.poll;
 
-/**
- * Copyright(c) 2001 iSavvix Corporation (http://www.isavvix.com/)
- *
- *                        All rights reserved
- *
- * Permission to use, copy, modify and distribute this material for
- * any purpose and without fee is hereby granted, provided that the
- * above copyright notice and this permission notice appear in all
- * copies, and that the name of iSavvix Corporation not be used in
- * advertising or publicity pertaining to this material without the
- * specific, prior written permission of an authorized representative of
- * iSavvix Corporation.
- *
- * ISAVVIX CORPORATION MAKES NO REPRESENTATIONS AND EXTENDS NO WARRANTIES,
- * EXPRESS OR IMPLIED, WITH RESPECT TO THE SOFTWARE, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR ANY PARTICULAR PURPOSE, AND THE WARRANTY AGAINST
- * INFRINGEMENT OF PATENTS OR OTHER INTELLECTUAL PROPERTY RIGHTS.  THE
- * SOFTWARE IS PROVIDED "AS IS", AND IN NO EVENT SHALL ISAVVIX CORPORATION OR
- * ANY OF ITS AFFILIATES BE LIABLE FOR ANY DAMAGES, INCLUDING ANY
- * LOST PROFITS OR OTHER INCIDENTAL OR CONSEQUENTIAL DAMAGES RELATING
- * TO THE SOFTWARE.
- *
- */
-
-
-import java.sql.*;
-
-import java.util.Properties;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Vector;
 
 
-/**
- * Manages a java.sql.Connection pool.
- *
- * @author  Anil Hemrajani
- */
-public class DBConnectionMgr {
-    private Vector connections = new Vector(10);
-    private String _driver = "oracle.jdbc.driver.OracleDriver";
-   private String _url = "jdbc:oracle:thin:@localhost:1521:XE";
-   private String _user = "sideon";
-    private String _password = "sideon";
-    
-    private boolean _traceOn = false;
-    private boolean initialized = false;
-    private int _openConnections = 50;
-    private static DBConnectionMgr instance = null;
-
-    public DBConnectionMgr() {
-    }
-
-    /** Use this method to set the maximum number of open connections before
-     unused connections are closed.
-     */
-
-    public static DBConnectionMgr getInstance() {
-        if (instance == null) {
-            synchronized (DBConnectionMgr.class) {
-                if (instance == null) {
-                    instance = new DBConnectionMgr();
-                }
-            }
-        }
-
-        return instance;
-    }
-
-    public void setOpenConnectionCount(int count) {
-        _openConnections = count;
-    }
-
-
-    public void setEnableTrace(boolean enable) {
-        _traceOn = enable;
-    }
-
-
-    /** Returns a Vector of java.sql.Connection objects */
-    public Vector getConnectionList() {
-        return connections;
-    }
-
-
-    /** Opens specified "count" of connections and adds them to the existing pool */
-    public synchronized void setInitOpenConnections(int count)
-            throws SQLException {
-        Connection c = null;
-        ConnectionObject co = null;
-
-        for (int i = 0; i < count; i++) {
-            c = createConnection();
-            co = new ConnectionObject(c, false);
-
-            connections.addElement(co);
-            trace("ConnectionPoolManager: Adding new DB connection to pool (" + connections.size() + ")");
-        }
-    }
-
-
-    /** Returns a count of open connections */
-    public int getConnectionCount() {
-        return connections.size();
-    }
-
-
-    /** Returns an unused existing or new connection.  */
-    public synchronized Connection getConnection()
-            throws Exception {
-        if (!initialized) {
-            Class c = Class.forName(_driver);
-            DriverManager.registerDriver((Driver) c.newInstance());
-
-            initialized = true;
-        }
-
-
-        Connection c = null;
-        ConnectionObject co = null;
-        boolean badConnection = false;
-
-
-        for (int i = 0; i < connections.size(); i++) {
-            co = (ConnectionObject) connections.elementAt(i);
-
-            // If connection is not in use, test to ensure it's still valid!
-            if (!co.inUse) {
-                try {
-                    badConnection = co.connection.isClosed();
-                    if (!badConnection)
-                        badConnection = (co.connection.getWarnings() != null);
-                } catch (Exception e) {
-                    badConnection = true;
-                    e.printStackTrace();
-                }
-
-                // Connection is bad, remove from pool
-                if (badConnection) {
-                    connections.removeElementAt(i);
-                    trace("ConnectionPoolManager: Remove disconnected DB connection #" + i);
-                    continue;
-                }
-
-                c = co.connection;
-                co.inUse = true;
-
-                trace("ConnectionPoolManager: Using existing DB connection #" + (i + 1));
-                break;
-            }
-        }
-
-        if (c == null) {
-            c = createConnection();
-            co = new ConnectionObject(c, true);
-            connections.addElement(co);
-
-            trace("ConnectionPoolManager: Creating new DB connection #" + connections.size());
-        }
-
-        return c;
-    }
-
-
-    /** Marks a flag in the ConnectionObject to indicate this connection is no longer in use */
-    public synchronized void freeConnection(Connection c) {
-        if (c == null)
-            return;
-
-        ConnectionObject co = null;
-
-        for (int i = 0; i < connections.size(); i++) {
-            co = (ConnectionObject) connections.elementAt(i);
-            if (c == co.connection) {
-                co.inUse = false;
-                break;
-            }
-        }
-
-        for (int i = 0; i < connections.size(); i++) {
-            co = (ConnectionObject) connections.elementAt(i);
-            if ((i + 1) > _openConnections && !co.inUse)
-                removeConnection(co.connection);
-        }
-    }
-
-    public void freeConnection(Connection c, PreparedStatement p, ResultSet r) {
-        try {
-            if (r != null) r.close();
-            if (p != null) p.close();
-            freeConnection(c);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void freeConnection(Connection c, Statement s, ResultSet r) {
-        try {
-            if (r != null) r.close();
-            if (s != null) s.close();
-            freeConnection(c);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void freeConnection(Connection c, PreparedStatement p) {
-        try {
-            if (p != null) p.close();
-            freeConnection(c);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void freeConnection(Connection c, Statement s) {
-        try {
-            if (s != null) s.close();
-            freeConnection(c);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /** Marks a flag in the ConnectionObject to indicate this connection is no longer in use */
-    public synchronized void removeConnection(Connection c) {
-        if (c == null)
-            return;
-
-        ConnectionObject co = null;
-        for (int i = 0; i < connections.size(); i++) {
-            co = (ConnectionObject) connections.elementAt(i);
-            if (c == co.connection) {
-                try {
-                    c.close();
-                    connections.removeElementAt(i);
-                    trace("Removed " + c.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                break;
-            }
-        }
-    }
-
-
-    private Connection createConnection()
-            throws SQLException {
-        Connection con = null;
-
-        try {
-            if (_user == null)
-                _user = "";
-            if (_password == null)
-                _password = "";
-
-            Properties props = new Properties();
-            props.put("user", _user);
-            props.put("password", _password);
-
-            con = DriverManager.getConnection(_url, props);
-        } catch (Throwable t) {
-            throw new SQLException(t.getMessage());
-        }
-
-        return con;
-    }
-
-
-    /** Closes all connections and clears out the connection pool */
-    public void releaseFreeConnections() {
-        trace("ConnectionPoolManager.releaseFreeConnections()");
-
-        Connection c = null;
-        ConnectionObject co = null;
-
-        for (int i = 0; i < connections.size(); i++) {
-            co = (ConnectionObject) connections.elementAt(i);
-            if (!co.inUse)
-                removeConnection(co.connection);
-        }
-    }
-
-
-    /** Closes all connections and clears out the connection pool */
-    public void finalize() {
-        trace("ConnectionPoolManager.finalize()");
-
-        Connection c = null;
-        ConnectionObject co = null;
-
-        for (int i = 0; i < connections.size(); i++) {
-            co = (ConnectionObject) connections.elementAt(i);
-            try {
-                co.connection.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            co = null;
-        }
-
-        connections.removeAllElements();
-    }
-
-
-    private void trace(String s) {
-        if (_traceOn)
-            System.err.println(s);
-    }
-
-}
-
-
-class ConnectionObject {
-    public java.sql.Connection connection = null;
-    public boolean inUse = false;
-
-    public ConnectionObject(Connection c, boolean useFlag) {
-        connection = c;
-        inUse = useFlag;
-    }
+public class PollMgr {
+	private DBConnectionMgr pool;
+	
+	public PollMgr() {
+		pool=DBConnectionMgr.getInstance();
+	}
+	
+	/**
+	 * 가장 최신 투표가 제일 위에 출력
+	 * @return
+	 */
+	public int getMaxNum() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int maxNum = 0;
+		try {
+			con = pool.getConnection();
+			sql = "select max(num) from tblPollList";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) maxNum = rs.getInt(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return maxNum;
+	}
+	
+	/**
+	 * 투표만들기
+	 * @param plBean
+	 * @param piBean
+	 * @return
+	 */
+	public boolean insertPoll(PollListBean plBean, PollItemBean piBean) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			con = pool.getConnection();
+			sql = "insert into tblpolllist(num, question,sdate,edate,type) values(TBLPOLLLIST_SEQ.NEXTVAL, ?, sysdate, ?, ?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, plBean.getQuestion());
+			pstmt.setString(2, plBean.getEdate());
+			pstmt.setInt(3, plBean.getType());//1은 복수투표, 0은 단일투표
+			int cnt = pstmt.executeUpdate();
+			pstmt.close();
+			if(cnt==1) {
+				sql="insert into tblPollItem values(?,?,?,0)";
+				pstmt = con.prepareStatement(sql);
+				int listNum = getMaxNum();
+				String item[] = piBean.getItem();
+				for (int i = 0; i < item.length; i++) {
+					if(item[i]==null || item[i].trim().equals("")) 
+						break;
+					pstmt.setInt(1,listNum);
+					pstmt.setInt(2, i);
+					pstmt.setString(3, item[i]);
+					if(pstmt.executeUpdate()==1)
+						flag=true;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return flag;
+	}
+	
+		/**
+		 * 투표 항목 갯수 조회
+		 * @param listNum
+		 * @return
+		 */
+		public int sumCount (int listNum) {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			int sum=0;
+			try {
+				con = pool.getConnection();
+				sql = "select sum(count) from tblPollItem where listnum=?";
+				pstmt = con.prepareStatement(sql);
+				if(listNum==0) listNum = getMaxNum();
+				pstmt.setInt(1, listNum);
+				rs = pstmt.executeQuery();
+				if(rs.next()) sum = rs.getInt(1);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
+			}
+			return sum;
+		}
+	
+		/**
+		 * 투표 정렬
+		 * @return
+		 */
+		public Vector<PollListBean> getPollList(){
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			Vector<PollListBean> vlist = new Vector<PollListBean>();
+			try {
+				con = pool.getConnection();
+				sql = "select * from tblPollList order by num desc";
+				pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					PollListBean plBean = new PollListBean();
+					plBean.setNum(rs.getInt("num"));
+					plBean.setQuestion(rs.getString("question"));
+					plBean.setSdate(rs.getString("sdate"));
+					plBean.setEdate(rs.getString("edate"));
+					vlist.addElement(plBean);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
+			}
+			return vlist;
+		}
+		
+		/**
+		 * 투표 리스트 조회
+		 * @param num
+		 * @return
+		 */
+		public PollListBean getPoll(int num) {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			PollListBean plBean  = new PollListBean(); 
+			try {
+				con = pool.getConnection();
+				sql = "select num, question, type, active from tblPollList where num=?";
+				pstmt = con.prepareStatement(sql);
+				if(num==0)
+					num = getMaxNum();
+				pstmt.setInt(1, num);
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					plBean.setNum(rs.getInt(1));
+					plBean.setQuestion(rs.getString(2));
+					plBean.setType(rs.getInt(3));
+					plBean.setActive(rs.getInt(4));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
+			}
+			return plBean;
+		}
+	
+		/**
+		 * 투표할 설문(투표) 선택
+		 * @param listNum
+		 * @return
+		 */
+		public Vector<String> getItemList(int listNum){
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			Vector<String> vlist = new Vector<String>();
+			try {
+				con = pool.getConnection();
+				sql = "select item from tblPollItem where listNum=?";
+				pstmt = con.prepareStatement(sql);
+				if(listNum==0) listNum = getMaxNum();
+				pstmt.setInt(1, listNum);
+				rs=pstmt.executeQuery();
+				while(rs.next()) {
+					vlist.addElement(rs.getString(1));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
+			}
+			return vlist;
+		}
+		
+		/**
+		 * 투표 인원 수 증가
+		 * @param listNum
+		 * @param itemNum
+		 * @return
+		 */
+		public boolean updatePoll(int listNum, String itemNum[]) {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			String sql = null;
+			boolean flag=false;
+			try {
+				con = pool.getConnection();
+				sql = "update tblPollItem set count = count+1 " 
+						+"where listNum=? and itemNum=?";
+				pstmt = con.prepareStatement(sql);
+				if(listNum==0) listNum=getMaxNum();
+				for (int i = 0; i < itemNum.length; i++) {
+					pstmt.setInt(1, listNum);
+					pstmt.setInt(2, Integer.parseInt(itemNum[i]));
+					if(pstmt.executeUpdate()==1)
+						flag=true;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt);
+			}
+			return flag;
+		}
+	    
+		/**
+		 * 각 항목 투표 인원 수 조회
+		 * @param listNum
+		 * @return
+		 */
+		public Vector<PollItemBean> getView(int listNum){
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			Vector<PollItemBean> vlist= new Vector<PollItemBean>();
+			try {
+				con = pool.getConnection();
+				sql = "select item, count from tblPollItem where listNum=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, listNum==0?getMaxNum():listNum);
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					PollItemBean piBean = new PollItemBean();
+					String item[]=new String[1];
+					item[0]=rs.getString("item");
+					piBean.setItem(item);
+					piBean.setCount(rs.getInt("count"));
+					vlist.addElement(piBean);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
+			}
+			return vlist;
+		}
+	    
+		/**
+		 * 투표 항목 중 가장 많이 투표된 항목 조회
+		 * @param listNum
+		 * @return
+		 */
+		public int getMaxCount(int listNum) {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			int maxCnt = 0;
+			try {
+				con = pool.getConnection();
+				sql = "select max(count) from tblPollItem where listNum=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, listNum==0?getMaxNum():listNum);
+				rs = pstmt.executeQuery();
+				if(rs.next()) maxCnt = rs.getInt(1);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
+			}
+			return maxCnt;
+		}
+		
 }
